b253591e61268bcc3e72ca43cb5ed262d4035148
likunru <kunru.li@capitalonline.net>
2018-10-29 18:36:02 +0800

    初始化

---
+++ chat/app.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const app = express();
+
+// 默认用户名
+const SYSTEM = '系统';
+
+// 设置静态文件夹，会默认找当前目录下的index.html文件当做访问的页面
+app.use(express.static(__dirname));
+
+
+// 用来保存对应的socket,就是记录对方的socket实例
+let socketObj = {};
+// 设置一些颜色的数组，让每次进入聊天的用户颜色都不一样
+let userColor = ['#00a1f4', '#0cc', '#f44336', '#795548', '#e91e63', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#ffc107', '#607d8b', '#ff9800', '#ff5722'];
+
+// 乱序排列方法
+function shuffle(arr) {
+    let len = arr.length;
+    while(0 !== len) {
+        // 右移位运算符向下取整
+        random = (Math.random() * len--) >>> 0;
+        // 解构赋值实现变量互换
+        [arr[len], arr[random]] = [arr[random], arr[len]];
+    }
+}
+// websocket是依赖http协议进行握手的
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+// 监听与客户端的链接事件
+io.on('connection', socket => {
+    console.log('服务端链接成功');
+    // 记录用户名，用来记录是不是第一次进入，默认为undefined
+    let username;
+    let color; //  用于存放颜色的变量
+    let rooms = []; //记录进入了那些房间的数组
+    //监听客服端发过来的信息
+    socket.on('message', msg => {
+        // 服务端发送message事件，把msg消息再发送给客户端
+        if (username) {
+            // 正则判断消息是否为私聊专属
+            let private = msg.match(/@([^] +)(.+)/);
+            if (private) { // 私聊信息
+                // 私聊用户，正则匹配的第一个分组
+                let toUser = private[1];
+                // 私聊的内容，正则匹配的第二个分组
+                let content = private[2];
+                // 从socketObj中获取私聊用户的socket
+                let socket = socketObj[toUser];
+                
+                if (toSocket) {
+                    // 向私聊的用户发消息
+                    toSocket.send({
+                        user: username,
+                        color,
+                        content,
+                        createAt: new Date().toLocaleString()
+                    })
+                }
+            } else { // 公聊信息
+                io.emit('message', {
+                    user: username,
+                    color,
+                    content: msg,
+                    createAt: new Date().toLocaleString()
+                })    
+            }
+             
+        } else {
+            username = msg;
+            socketObj[username] = socket;
+            // 乱序后取出颜色数组中的第一个，分配给进入的用户
+            color = shuffle(userColor)[0];
+            // 向除了自己的所有人广播，
+            socket.broadcast.emit('message', {
+                user: SYSTEM,
+                color,
+                constent: '${username}加入了聊天',
+                createAt: new Date().toLocaleDateString()
+            })
+        }
+        
+    });
+    // 监听进入房间事件
+    socket.on('join', room => {
+        // 判断用户是否进入房间，如果没有就让其进入房间
+        if (username && rooms.indexOf(room) === -1) {
+            // socket.join表示进入某个房间
+            socket.join(room);
+            rooms.push(room);
+            // 这里发送个joined事件，让前端监听后，控制房间按钮显隐
+            socket.emit('joined', room);
+            // 通知一下自己
+            socket.send({
+                user: SYSTEM,
+                color,
+                content: '你已加入${room}战队',
+                createAt: new Date().toLocaleString
+            })
+        }
+    });
+    // 监听离开房间事件
+    socket.on('leave', room => {
+        // index 当前房间rooms数组的索引
+        let index = rooms.indexOf(room);
+        if (index !== -1) {
+            socket.leave(room); //离开改房间
+            rooms.splice(index, 1); // 将用户从改房间里删除
+            // 向前台发送一个leaved事件
+            socket.emit('leaved', room);
+            socket.send({
+                user: SYSTEM,
+                color,
+                content: '你离开${room}战队',
+                createAt: new Date().toLocaleString
+            })
+        }
+    })
+})
+
+server.listen(4000);
---
+++ chat/index.js
@@ -0,0 +1,80 @@
+let socket = io();
+// 监听与服务端的链接
+socket.on('connect', () => {
+    console.log('链接成功');
+})
+
+// 获取列表list，输入框content,按钮sendBtn元素
+let list = document.getElementById('list'),
+    input = document.getElementById('input');
+    sendBtn = document.getElementById('sendBtn');
+
+// 发送信息
+function send() {
+    let value = input.value;
+    if (value) {
+        socket.emit('message', value);
+        input.value = '';
+    } else {
+        alert('输入内容不能为空!');
+    }
+}
+sendBtn.onclick = send;    
+
+// 回车发送信息
+function enterSend(event) {
+    let code = event.keyCode;
+    if (code === 13) send();
+}
+
+input.onkeydown = function (event) {
+    enterSend(event);
+}
+
+// 监听message事件来接收服务端发过来的消息
+socket.on('message', data => {
+    // 创建新的li元素，最终将其添加到list列表
+    let li = document.createElement('li');
+    console.log(111, data);
+    li.className = 'list-group-name';
+    li.innerHTML = '<p style="color: #ccc;"><span class="user" style="color:'+ data.color 
+      + ';">' + data.user + '</span>' + data.createAt + '</p><p class="content" style="color:' + data.color +';">' + data.content + '</p>';
+    list.appendChild(li);
+    // 讲聊天区的滚动条设置到最新内容的位置
+    list.scrollTop = list.scrollHeight;
+})
+
+// 实现私聊的方法
+function privateChat (event) {
+    let target = event.target;
+    // 拿到对应的用户名
+    let user = target.innerHTML;
+    if (target.className === 'user') {
+        input.value = '@' + user;
+    }
+}
+list.onclick = function(event) {
+    privateChat(event);
+}
+
+// 进入房间的方法
+function join(room) {
+    socket.emit('join', room);
+}
+
+// 监听是否进入房间，如果进入房间，就显示离开房间的按钮
+socket.on('joined', room => {
+    document.getElementById('join-' + room).style.display = 'none';
+    document.getElementById('leave-' + room).style.display = 'inline-block';
+})
+
+// 离开房间的方法
+function leave(room) {
+    socket.emit('leave', room);
+}
+
+// 监听是否已离开房间，如果离开房间，就显示进入房间的按钮
+socket.on('leave', room => {
+    document.getElementById('leave-' + room).style.display = 'none';
+    document.getElementById('join-' + room).style.display = 'inline-block';
+})
